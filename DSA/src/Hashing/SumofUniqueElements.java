3f874ee1a6f3b15ecbcb9a4e801beb353c8443a0
nagal <nagalakshmi3105@gmail.com>
2022-03-03 09:03:01 +0530

    hash

---
+++ DSA/src/Hashing/SumofUniqueElements.java
@@ -0,0 +1,31 @@
+package Hashing;
+
+import java.util.HashMap;
+
+public class SumofUniqueElements {
+	public static void main(String args[])
+	{
+		int nums[] = {1,2,3,2};
+		int res = sumOfUnique(nums);
+		System.out.println(res);
+		
+	}
+	public static int sumOfUnique(int[] nums) {
+        
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<nums.length;i++)
+        {
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        }
+        
+        int sum= 0;
+        for(int val : nums)
+        {
+            if(map.get(val)==1)
+                sum +=val;
+        }
+        return sum;
+ 
+
+}
+}
